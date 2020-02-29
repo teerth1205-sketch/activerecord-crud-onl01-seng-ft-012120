c1ed1e5c2311bf7c2825f6e061f4e1763529cd6e
Teerth Patel <teerth1205@gmail.com>
2020-02-29 02:27:22 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -28,7 +28,7 @@ def can_be_created_in_a_block(arg = {title: "Home Alone", release_date: 1990})
   # If no arguments are passed, use default values:
   # title == "Home Alone"
   # release_date == 1990
-  binding.pry
+  
   Movie.create do |m|
     m.title = arg[:title]
     m.release_date = arg[:release_date]
